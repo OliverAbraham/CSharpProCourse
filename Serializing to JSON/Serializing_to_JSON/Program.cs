530c322e99e7edd5b82f3df8d015c417ad978dae
Oliver Abraham <mail@oliver-abraham.de>
2022-02-04 18:07:46 +0100

    serialization

---
+++ Serializing to JSON/Serializing_to_JSON/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+Console.WriteLine("Serializing and deserializing data to and from JSON");
+
+
+var myData         = new MyData();
+myData.Name        = "Oliver";
+myData.Description = "A person on this planet";
+myData.Amount      = 123.456m;
+
+Console.WriteLine($"My data that I want to save:");
+Console.WriteLine($"myData.Name        = '{myData.Name       }'");
+Console.WriteLine($"myData.Description = '{myData.Description}'");
+Console.WriteLine($"myData.Amount      = '{myData.Amount     }'");
+
+Console.WriteLine($"\nWriting this data to a file");
+
+var jsonToFile = JsonConvert.SerializeObject(myData);
+File.WriteAllText("JsonSerializerDemo.dat", jsonToFile);
+
+
+Console.WriteLine($"\nReading data back from file");
+
+var jsonFromFile = File.ReadAllText("JsonSerializerDemo.dat");
+
+Console.WriteLine($"\nConverting back to my class");
+
+var myDataBack = JsonConvert.DeserializeObject<MyData>(jsonFromFile);
+
+Console.WriteLine($"\nThe result:");
+Console.WriteLine($"myData.Name        = '{myDataBack.Name       }'");
+Console.WriteLine($"myData.Description = '{myDataBack.Description}'");
+Console.WriteLine($"myData.Amount      = '{myDataBack.Amount     }'");
+Console.WriteLine($"Press a key to end");
+Console.ReadKey();
+
+public class MyData
+{
+    public string  Name        { get; set; }
+    public string  Description { get; set; }
+    public decimal Amount      { get; set; }
+}
+
---
+++ Serializing to XML/Serializing_to_XML/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Xml.Serialization;
+
+Console.WriteLine("Serializing and deserializing data to and from XML");
+
+
+
+var myData         = new MyData();
+myData.Name        = "Oliver";
+myData.Description = "A person on this planet";
+myData.Amount      = 123.456m;
+
+Console.WriteLine($"My data that I want to save:");
+Console.WriteLine($"myData.Name        = '{myData.Name       }'");
+Console.WriteLine($"myData.Description = '{myData.Description}'");
+Console.WriteLine($"myData.Amount      = '{myData.Amount     }'");
+
+
+Console.WriteLine($"\nWriting this data to a file");
+
+var serializer = new XmlSerializer(typeof(MyData));
+var stream = new FileStream("MyFilename.xml", FileMode.Create);
+serializer.Serialize(stream, myData);
+stream.Close();
+
+
+Console.WriteLine($"\nReading data back from file");
+
+stream = new FileStream("MyFilename.xml", FileMode.Open);
+MyData myDataBack = (MyData)serializer.Deserialize(stream);
+stream.Close();
+
+
+Console.WriteLine($"\nThe result:");
+Console.WriteLine($"myData.Name        = '{myDataBack.Name       }'");
+Console.WriteLine($"myData.Description = '{myDataBack.Description}'");
+Console.WriteLine($"myData.Amount      = '{myDataBack.Amount     }'");
+Console.WriteLine($"Press a key to end");
+Console.ReadKey();
+
+
+public class MyData
+{
+    public string  Name        { get; set; }
+    public string  Description { get; set; }
+    public decimal Amount      { get; set; }
+}
+
