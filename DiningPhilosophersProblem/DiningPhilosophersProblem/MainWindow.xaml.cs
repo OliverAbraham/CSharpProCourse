a9d09f941bd04d1e0c2231ddbe4e618f40f03d7e
Oliver Abraham <mail@oliver-abraham.de>
2022-03-18 16:51:03 +0100

    DiningPhilosophersProblem

---
+++ DiningPhilosophersProblem/DiningPhilosophersProblem/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Threading;
+using System.Windows;
+
+namespace DiningPhilosophersProblem
+{
+	public partial class MainWindow : Window
+	{
+        #region Fields
+        private Philosopher[] _Philosophers;
+        private Thread _StopperThread;
+        private bool _Closing_is_in_progress;
+        private bool _Closing_should_cancel;
+        #endregion
+
+        #region Initialisation
+        public MainWindow()
+        {
+            InitializeComponent();
+            Initialize_philosophers();
+        }
+
+        private void Initialize_philosophers()
+        {
+            _Philosophers = new Philosopher[5];
+            _Philosophers[0] = new Philosopher(Dispatcher, Plate1, Fork5, Fork1);
+            _Philosophers[1] = new Philosopher(Dispatcher, Plate2, Fork1, Fork2);
+            _Philosophers[2] = new Philosopher(Dispatcher, Plate3, Fork2, Fork3);
+            _Philosophers[3] = new Philosopher(Dispatcher, Plate4, Fork3, Fork4);
+            _Philosophers[4] = new Philosopher(Dispatcher, Plate5, Fork4, Fork5);
+        }
+        #endregion
+
+        #region Program shutdown
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!_Closing_is_in_progress)
+            {
+                Stop_all_workers_wait_and_then_close(e);
+                e.Cancel = true;
+                _Closing_is_in_progress = true;
+                _Closing_should_cancel = true;
+            }
+            else
+            {
+                e.Cancel = _Closing_should_cancel;
+            }
+        }
+
+        private void Stop_all_workers_wait_and_then_close(System.ComponentModel.CancelEventArgs e)
+        {
+            Title = "Stopping the program...";
+            _StopperThread = new Thread(new ParameterizedThreadStart(StopperThreadProcedure));
+            _StopperThread.Start();
+        }
+
+        private void StopperThreadProcedure(object obj)
+        {
+            Stop_all_workers_and_wait();
+            Close_program();
+        }
+
+        private void Stop_all_workers_and_wait()
+        {
+            foreach (var p in _Philosophers)
+            {
+                if (p != null) p.Stop();
+            }
+        }
+
+        private void Close_program()
+        {
+            _Closing_should_cancel = false;
+            Dispatcher.Invoke(() => Close() );
+        }
+        #endregion
+	}
+}
---
+++ DiningPhilosophersProblem/DiningPhilosophersProblem/Philosopher.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace DiningPhilosophersProblem
+{
+    internal enum ActionState
+    {
+        Thinking,
+        Eating,
+        Sleeping
+    }
+
+    internal class Philosopher
+    {
+        #region Resources
+        private Button      Plate;
+        private Button      Left_fork;
+        private Button      Right_fork;
+        #endregion
+
+        #region Working stuff
+        private ActionState State;
+        private Thread      WorkerThread;
+        private bool        WorkerShouldStop;
+        private bool        WorkerHasStopped;
+        private Dispatcher  Dispatcher;
+        private Random      RandomTimeGenerator;
+        #endregion
+
+        #region Construction
+        public Philosopher(Dispatcher dispatcher, Button plate, Button left_fork, Button right_fork)
+        {
+            Dispatcher          = dispatcher;
+            Plate               = plate;
+            Left_fork           = left_fork;
+            Right_fork          = right_fork;
+
+            RandomTimeGenerator = new Random((int)DateTime.Now.Ticks);
+            WorkerShouldStop    = false;
+            WorkerHasStopped    = false;
+            WorkerThread        = new Thread(new ParameterizedThreadStart(ThreadProcedure));
+            WorkerThread.Start();
+        }
+        #endregion
+
+        #region Methods
+        internal void Stop()
+        {
+            Tell_worker_to_stop();
+            Wait_until_worker_has_stopped();
+        }
+        #endregion
+
+        #region Implementation
+        private void ThreadProcedure(object obj)
+        {
+            while(true)
+            {
+                Think();
+                if (WorkerShouldStop) break;
+
+                Eat();
+                if (WorkerShouldStop) break;
+
+                Sleep();
+                if (WorkerShouldStop) break;
+            }
+
+            WorkerHasStopped = true;
+        }
+
+        private void Tell_worker_to_stop()
+        {
+            WorkerShouldStop = true;
+        }
+
+        private void Wait_until_worker_has_stopped()
+        {
+            while (!WorkerHasStopped)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        private void Wait()
+        {
+            int milliseconds = RandomTimeGenerator.Next(minValue: 2000, maxValue:5000);
+            Thread.Sleep(milliseconds);
+        }
+
+        private void Think()
+        {
+            State = ActionState.Thinking;
+            Dispatcher.Invoke(() => 
+            {
+                Plate.Content = "T";
+                Plate.Background = Brushes.LightGreen;
+            });
+            Wait();
+        }
+
+        private void Sleep()
+        {
+            State = ActionState.Sleeping;
+            Dispatcher.Invoke(() => 
+            {
+                Plate.Content = "S";
+                Plate.Background = Brushes.Gray;
+            });
+            Wait();
+        }
+
+        private void Eat()
+        {
+            Try_to_grab_both_forks();
+            Start_eating();
+            Wait();
+            Release_forks();
+        }
+
+        private void Try_to_grab_both_forks()
+        {
+            Wait_while_a_fork_is_in_use();
+            Grab_both_forks();
+        }
+
+        private void Wait_while_a_fork_is_in_use()
+        {
+            while (ForkIsInUse(Left_fork) ||
+                   ForkIsInUse(Right_fork))
+            {
+                Thread.Sleep(100);
+                if (WorkerShouldStop) 
+                    break;
+            }
+        }
+
+        private void Grab_both_forks()
+        {
+            Dispatcher.Invoke(() => 
+            {
+                Left_fork .Background = Brushes.Red;
+                Right_fork.Background = Brushes.Red;
+            });
+        }
+
+        private void Release_forks()
+        {
+            Dispatcher.Invoke(() => 
+            {
+                Left_fork .Background = Brushes.LightGray;
+                Right_fork.Background = Brushes.LightGray;
+            });
+        }
+
+        private bool ForkIsInUse(Button fork)
+        {
+            bool Result = false;
+            Dispatcher.Invoke(() => 
+            {
+                Result = fork.Background == Brushes.Red;
+            });
+            return Result;
+        }
+
+        private void Start_eating()
+        {
+            State = ActionState.Eating;
+            Dispatcher.Invoke(() => 
+            {
+                Plate.Content = "E";
+                Plate.Background = Brushes.Red;
+            });
+        }
+        #endregion
+   }
+}
