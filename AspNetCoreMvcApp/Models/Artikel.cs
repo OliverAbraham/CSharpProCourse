1cff202fa9703902943b4f3f895f6939a3f583ec
Oliver Abraham <mail@oliver-abraham.de>
2022-04-16 16:41:45 +0200

    Einkaufsliste ASP.NETCore MVC

---
+++ AspNetCoreMvcApp/Models/Artikel.cs
@@ -0,0 +1,8 @@
+﻿namespace AspNetCoreMvcApp.Models
+{
+	public class Artikel
+	{
+		public int ID { get; set; }
+		public string Bezeichnung { get; set; }
+	}
+}
---
+++ AspNetCoreMvcApp/Models/Einkaufszettel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AspNetCoreMvcApp.Models
+{
+	public class Einkaufszettel
+	{
+		public string NeuerArtikel { get; set; }
+
+		public List<Artikel> AlleArtikel { get; set; }
+	}
+}
