01dbcb2717190c3b726552b7cd795c8414e87e34
Oliver Abraham <mail@oliver-abraham.de>
2022-08-18 10:41:47 +0200

    upgraded some projects to net6.0

--- OneDrive API/OneDriveApiTest.WinForms/Form1.cs
+++ OneDrive API/OneDriveApiTest.WinForms/Form1.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.OneDrive.Sdk;
-using Microsoft.OneDrive.Sdk.WindowsForms;
+//using Microsoft.OneDrive.Sdk.WindowsForms;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -33,82 +33,82 @@ namespace OneDriveApiTest.WinForms
 
         private static async Task Connect()
         {
-            string   clientId  = "000000004817ED58";
-            string   returnUrl = "https://login.live.com/oauth20_desktop.srf";
-            string[] scopes    = { "onedrive.readwrite", "wl.offline_access", "wl.signin" };
-
-            Client = OneDriveClient.GetMicrosoftAccountClient(
-                         clientId,
-                         returnUrl,
-                         scopes, webAuthenticationUi: new FormsWebAuthenticationUi());
-
-
-            try
-            {
-                if (!Client.IsAuthenticated)
-                {
-                    await Client.AuthenticateAsync();
-                }
-
-                //var drive = await Client.Drive.Request().GetAsync();
-
-                await LoadFolderFromPath();
-
-                //UpdateConnectedStateUx(true);
-            }
-            catch (OneDriveException exception)
-            {
-                // Swallow authentication cancelled exceptions
-                if (!exception.IsMatch(OneDriveErrorCode.AuthenticationCancelled.ToString()))
-                {
-                    if (exception.IsMatch(OneDriveErrorCode.AuthenticationFailure.ToString()))
-                    {
-                        MessageBox.Show("Authentication failed", "Authentication failed",MessageBoxButtons.OK);
-                        var httpProvider = Client.HttpProvider as HttpProvider;
-                        httpProvider.Dispose();
-                        Client = null;
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-            }
+ //           string   clientId  = "000000004817ED58";
+ //           string   returnUrl = "https://login.live.com/oauth20_desktop.srf";
+ //           string[] scopes    = { "onedrive.readwrite", "wl.offline_access", "wl.signin" };
+ //
+ //           Client = OneDriveClient.GetMicrosoftAccountClient(
+ //                        clientId,
+ //                        returnUrl,
+ //                        scopes, webAuthenticationUi: new FormsWebAuthenticationUi());
+ //
+ //
+ //           try
+ //           {
+ //               if (!Client.IsAuthenticated)
+ //               {
+ //                   await Client.AuthenticateAsync();
+ //               }
+ //
+ //               //var drive = await Client.Drive.Request().GetAsync();
+ //
+ //               await LoadFolderFromPath();
+ //
+ //               //UpdateConnectedStateUx(true);
+ //           }
+ //           catch (OneDriveException exception)
+ //           {
+ //               // Swallow authentication cancelled exceptions
+ //               if (!exception.IsMatch(OneDriveErrorCode.AuthenticationCancelled.ToString()))
+ //               {
+ //                   if (exception.IsMatch(OneDriveErrorCode.AuthenticationFailure.ToString()))
+ //                   {
+ //                       MessageBox.Show("Authentication failed", "Authentication failed",MessageBoxButtons.OK);
+ //                       var httpProvider = Client.HttpProvider as HttpProvider;
+ //                       httpProvider.Dispose();
+ //                       Client = null;
+ //                   }
+ //                   else
+ //                   {
+ //                       throw;
+ //                   }
+ //               }
+ //           }
         }
 
 
         private static async Task LoadFolderFromPath(string path = null)
         {
-            if (Client == null)
-                return;
-
-            try
-            {
-                Item folder;
-
-                var expandValue = Client.ClientType == ClientType.Consumer
-                    ? "thumbnails,children(expand=thumbnails)"
-                    : "thumbnails,children";
-
-                if (path == null)
-                {
-                    folder = await Client.Drive.Root.Request().Expand(expandValue).GetAsync();
-                }
-                else
-                {
-                    folder =
-                        await
-                            Client.Drive.Root.ItemWithPath("/" + path)
-                                .Request()
-                                .Expand(expandValue)
-                                .GetAsync();
-                }
-
-                ProcessFolder(folder);
-            }
-            catch (Exception)
-            {
-            }
+ //           if (Client == null)
+ //               return;
+ //
+ //           try
+ //           {
+ //               Item folder;
+ //
+ //               var expandValue = Client.ClientType == ClientType.Consumer
+ //                   ? "thumbnails,children(expand=thumbnails)"
+ //                   : "thumbnails,children";
+ //
+ //               if (path == null)
+ //               {
+ //                   folder = await Client.Drive.Root.Request().Expand(expandValue).GetAsync();
+ //               }
+ //               else
+ //               {
+ //                   folder =
+ //                       await
+ //                           Client.Drive.Root.ItemWithPath("/" + path)
+ //                               .Request()
+ //                               .Expand(expandValue)
+ //                               .GetAsync();
+ //               }
+ //
+ //               ProcessFolder(folder);
+ //           }
+ //           catch (Exception)
+ //           {
+ //           }
         }
 
         private static void ProcessFolder(Item folder)
--- SignalR/WpfClient/MainWindow.xaml.cs
+++ SignalR/WpfClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.SignalR.Client;
+﻿//using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNetCore.SignalR.Client;
 using System.Net.Http;
 using System.Windows;
 
@@ -14,7 +15,7 @@ namespace WpfClient
     public partial class MainWindow : Window
     {
         private HubConnection Connection;
-        private IHubProxy HubProxy;
+//        private IHubProxy HubProxy;
 
         public MainWindow()
         {
@@ -24,27 +25,27 @@ namespace WpfClient
 
         private async void Connect()
         {
-            Connection = new HubConnection("http://localhost:61541/DemoHub"); 
+//            Connection = new HubConnection("http://localhost:61541/DemoHub"); 
             //Connection.Closed += Connection_Closed; 
-            HubProxy = Connection.CreateHubProxy("demohub"); 
-            //Handle incoming event from server: use Invoke to write to console from SignalR's thread 
-            HubProxy.On<string, string>("ReceiveMessage", (name, message) => 
-                this.Dispatcher.Invoke(() => Messages.Items.Add($"{name}: {message}"))
-                ); 
-            
-            try 
-            { 
-                await Connection.Start(); 
-            } 
-            catch (HttpRequestException) 
-            { 
-                MessageBox.Show("Unable to connect to server: Start server before connecting clients.");
-            } 
+//            HubProxy = Connection.CreateHubProxy("demohub"); 
+//            //Handle incoming event from server: use Invoke to write to console from SignalR's thread 
+//            HubProxy.On<string, string>("ReceiveMessage", (name, message) => 
+//                this.Dispatcher.Invoke(() => Messages.Items.Add($"{name}: {message}"))
+//                ); 
+//            
+//            try 
+//            { 
+//                await Connection.Start(); 
+//            } 
+//            catch (HttpRequestException) 
+//            { 
+//                MessageBox.Show("Unable to connect to server: Start server before connecting clients.");
+//            } 
         }
 
         private void Button_Send_Click(object sender, RoutedEventArgs e)
         {
-             HubProxy.Invoke("SendMessage", "User", textBoxMessage.Text);
+//             HubProxy.Invoke("SendMessage", "User", textBoxMessage.Text);
         }
     }
 }
