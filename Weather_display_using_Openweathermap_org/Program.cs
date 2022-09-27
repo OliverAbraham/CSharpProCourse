282c3bf75db793c7d47307862ed06cf72ff548f2
Oliver Abraham <mail@oliver-abraham.de>
2022-09-27 19:32:13 +0200

    New weather display using Openweathermap

---
+++ Weather_display_using_Openweathermap_org/Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace Weather_display_using_Openweathermap_org
+{
+    internal class Program
+    {
+        private static string apiKey = File.ReadAllText(@"C:\Cloud\Openweathermap-ApiKey.txt");
+
+        static void Main()
+        {
+            Console.WriteLine("Weather display using the API of Openweathermap.org");
+
+            GetCurrentWeather();
+
+            // Uncomment this to make periodic calls:
+            //var scheduler = new Abraham.Scheduler.Scheduler()
+            //    .UseIntervalMinutes(1)
+            //    .UseAction( ()=> GetCurrentWeather() )
+            //    .Start();
+            //
+            //Console.WriteLine("Press any key to end");
+            //Console.ReadKey();
+            //scheduler.Stop();                
+        }
+
+        private static void GetCurrentWeather()
+        {
+            var httpClient = new RestClient();
+            var request = new RestRequest($"https://api.openweathermap.org/data/3.0/onecall?lat=53.8667&lon=9.8833&lang=de&units=metric&appid={apiKey}", Method.Get);
+            var response = httpClient.Execute(request);
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine($"Request wasn't successful.\nMore info:{response.StatusDescription}");
+                return;
+            }
+
+            var myWeatherData = JsonConvert.DeserializeObject<Root>(response.Content);
+            Console.WriteLine($"Current temperature: {myWeatherData.current.temp} °C");
+        }
+    }
+}
