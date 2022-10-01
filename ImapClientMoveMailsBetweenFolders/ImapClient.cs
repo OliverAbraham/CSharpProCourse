3af3f1626a17ffa2deccb6dd29cfb6df3655525c
Oliver Abraham <mail@oliver-abraham.de>
2022-10-01 17:38:51 +0200

    Added ImapClientMoveMails demo

---
+++ ImapClientMoveMailsBetweenFolders/ImapClient.cs
@@ -0,0 +1,94 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailKit.Security;
+
+namespace ImapClientMoveMailsBetweenFolders
+{
+			// IMPORTANT !!!!
+			// The IMAP reader needs a codepage provider to decode different character sets
+			// for this, add the nuget package "System.Encoding.Text.Codepages"
+			// and call the Encoding.RegisterProvider method, like this:
+			// 
+
+	public class ImapClient
+    {
+		private string	   _hostname = "imap.1blu.de";
+		private int		   _port 	 = 993;
+		private bool	   _useSsl   = true;
+		private string	   _username = "c263677_1-mail";
+		public  string	   _password = "";
+		private MailKit.Net.Imap.ImapClient _client;
+
+
+		public ImapClient()
+		{
+		}
+
+		public void RegisterCodepageProvider()
+		{
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+		}
+
+		public void Open()
+		{
+            _client = new MailKit.Net.Imap.ImapClient();
+			_client.Connect (_hostname, _port, (_useSsl ? SecureSocketOptions.SslOnConnect : SecureSocketOptions.None));
+			_client.Authenticate (_username, _password);
+		}
+
+		public void Close()
+		{
+			_client.Disconnect (true);
+		}
+
+		public List<IMailFolder> GetAllFolders()
+        {
+			var results = new List<IMailFolder>();
+
+			var personal = _client.GetFolder(_client.PersonalNamespaces[0]);
+			foreach (var folder in personal.GetSubfolders (false))
+				results.Add (folder);
+
+			return results;
+        }
+
+		public IMailFolder GetFolderByName(List<IMailFolder> folders, string name, bool caseInsensitive = true)
+		{
+			if (caseInsensitive)
+				return folders.Where(x => x.Name.ToUpper() == name.ToUpper()).First();
+			else
+				return folders.Where(x => x.Name == name).First();
+		}
+
+		/// <summary>
+		/// Example showing:
+		///  - how to fetch all messages from an IMAP mailbox
+		/// </summary>
+		/// <param name="hostname">Hostname of the server. For example: pop3.live.com</param>
+		/// <param name="port">Host port to connect to. Normally: 110 for plain POP3, 995 for SSL POP3</param>
+		/// <param name="useSsl">Whether or not to use SSL to connect to server</param>
+		/// <param name="username">Username of the user on the server</param>
+		/// <param name="password">Password of the user on the server</param>
+		/// <returns>All Messages from the inbox</returns>
+		public List<Message> GetAllMessagesFromFolder(IMailFolder folder)
+        {
+			var results = new List<Message>();
+			folder.Open(FolderAccess.ReadOnly);
+
+			var uniqueIDs = folder.Search (SearchQuery.All);
+			foreach (var uniqueID in uniqueIDs) 
+				results.Add(new Message(uniqueID, _client.Inbox.GetMessage (uniqueID)));
+
+			folder.Close();
+			return results;
+        }
+
+		public void MoveEmailToFolder(Message email, IMailFolder source, IMailFolder destination)
+		{
+			source     .Open(FolderAccess.ReadWrite);
+			source.MoveTo(email.UID, destination);
+			source     .Close();
+		}
+	}
+}
---
+++ ImapClientMoveMailsBetweenFolders/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace ImapClientMoveMailsBetweenFolders
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("IMAP client - move an email from your inbox to a different folder");
+
+			//
+			// IMPORTANT !!!!
+			// add the nuget package MailKit from Jeffrey Stedfast (doku in http://www.mimekit.net)
+			//
+
+			var _client = new ImapClient();
+			Console.WriteLine("Enter the password for your email postbox: ");
+			_client._password = EnterPassword();
+			_client.Open();
+
+
+			Console.WriteLine("\n\n\nThese are your mailbox folders:");
+			var folders = _client.GetAllFolders();
+			folders.ForEach(x => Console.WriteLine($"    - {x.Name}"));
+
+
+			Console.WriteLine("\n\n\nReading the inbox...");
+			var inbox = _client.GetFolderByName(folders, "inbox");
+			var emails = _client.GetAllMessagesFromFolder(inbox);
+
+			Console.WriteLine("\n\n\nThese are the last 5 emails:");
+			var lastFiveEmails = emails.OrderByDescending(x => x.Msg.Date).Take(5).ToList();
+			folders.ForEach(x => Console.WriteLine($"    - {x}"));
+
+
+			Console.WriteLine("\n\n\nThe last email from your inbox is:");
+			var lastEmail = emails.Last();
+			Console.WriteLine($"    - {lastEmail}");
+
+
+			Console.Write("\n\n\nEnter the folder name to move the last email to: ");
+			var folderName = Console.ReadLine();
+			Console.WriteLine();
+			
+			
+			var destination = _client.GetFolderByName(folders, folderName);
+			if (destination is null)
+				Console.WriteLine("that folder doesn't exist!");
+
+
+			Console.Write("\n\nMoving the mail to that folder...");
+			_client.MoveEmailToFolder(lastEmail, inbox, destination);
+			Console.WriteLine("done");
+ 			_client.Close();
+		}
+
+		private static string EnterPassword()
+		{
+			var password = "";
+			while (true)
+			{
+				var key = Console.ReadKey(intercept: true);
+				if (key.KeyChar == '\r')
+					break;
+				password += key.KeyChar;
+				Console.Write('*');
+			}
+
+			return password;
+		}
+	}
+}
