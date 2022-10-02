bad3d8bf309bf627246b5d869f79eb7c75110454
Oliver Abraham <mail@oliver-abraham.de>
2022-10-02 13:39:15 +0200

    Factory pattern

---
+++ Pattern - Factory Pattern/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Pattern___Factory_Pattern
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // using a factory to "produce" objects
+            var _shapes = new List<Shape>();
+            _shapes.Add(ShapeFactory.CreateLine());
+            _shapes.Add(ShapeFactory.CreateTriangle());
+            _shapes.Add(ShapeFactory.CreateRectangle());
+            _shapes.Add(ShapeFactory.CreatePolygon());
+            _shapes.Add(ShapeFactory.CreateCircle());
+        }
+    }
+
+    public class ShapeFactory
+    {
+        public static Shape CreateLine()
+        {
+            var line = new Line() { X1 = 0, Y1 = 0, X2 = 100, Y2 = 150, StrokeThickness = 5, Stroke = Brushes.Black };
+            return line;
+        }
+
+        public static Shape CreateTriangle()
+        {
+            var polygon = new Polygon() { StrokeThickness = 1,  Stroke = Brushes.Black, Fill = Brushes.DarkBlue };
+            polygon.Points.Add(new Point(0  ,100));
+            polygon.Points.Add(new Point(100,100));
+            polygon.Points.Add(new Point(50 ,0  ));
+            polygon.Points.Add(new Point(0  ,100));
+            return polygon;
+        }
+
+        public static Shape CreateRectangle()
+        {
+            var polygon = new Polygon() { StrokeThickness = 1,  Stroke = Brushes.Black, Fill = Brushes.DarkRed };
+            polygon.Points.Add(new Point(0  ,100));
+            polygon.Points.Add(new Point(100,100));
+            polygon.Points.Add(new Point(100,  0));
+            polygon.Points.Add(new Point(0  ,  0));
+            return polygon;
+        }
+
+        public static Shape CreatePolygon()
+        {
+            var polygon = new Polygon() { StrokeThickness = 1,  Stroke = Brushes.Black, Fill = Brushes.DarkGreen };
+            polygon.Points.Add(new Point(  0,  0));
+            polygon.Points.Add(new Point( 20, 50));
+            polygon.Points.Add(new Point(  0,100));
+            polygon.Points.Add(new Point( 50, 80));
+            polygon.Points.Add(new Point(100,100));
+            polygon.Points.Add(new Point( 80, 50));
+            polygon.Points.Add(new Point(100,  0));
+            polygon.Points.Add(new Point( 50, 20));
+            polygon.Points.Add(new Point(  0,  0));
+            return polygon;
+        }
+
+        public static Shape CreateCircle()
+        {
+            var circle = new Ellipse() { Width = 100, Height = 100, StrokeThickness = 1,  Stroke = Brushes.Black, Fill = Brushes.DarkCyan };
+            return circle;
+        }
+    }
+}
--- Retry Pattern/Program.cs
+++ Retry Pattern/Program.cs
@@ -2,57 +2,55 @@
 using System;
 using System.Threading;
 
-namespace PollyTest
+namespace PollyTest;
+class Program
 {
-	class Program
+	// see https://github.com/App-vNext/Polly
+	// see http://www.thepollyproject.org
+	//
+	// Polly is a .NET resilience and transient-fault-handling library that allows developers
+	// to express policies such as Retry, Circuit Breaker, Timeout, Bulkhead Isolation, and
+	// Fallback in a fluent and thread-safe manner.
+	//
+	// Polly targets .NET Standard 1.1 (coverage: .NET Framework 4.5-4.6.1, .NET Core 1.0,
+	// Mono, Xamarin, UWP, WP8.1+) and .NET Standard 2.0+ (coverage: .NET Framework 4.6.1,
+	// .NET Core 2.0+, and later Mono, Xamarin and UWP targets).
+
+	static void Main()
 	{
-		// see https://github.com/App-vNext/Polly
-		// see http://www.thepollyproject.org
-		//
-		// Polly is a .NET resilience and transient-fault-handling library that allows developers
-		// to express policies such as Retry, Circuit Breaker, Timeout, Bulkhead Isolation, and
-		// Fallback in a fluent and thread-safe manner.
-		//
-		// Polly targets .NET Standard 1.1 (coverage: .NET Framework 4.5-4.6.1, .NET Core 1.0,
-		// Mono, Xamarin, UWP, WP8.1+) and .NET Standard 2.0+ (coverage: .NET Framework 4.6.1,
-		// .NET Core 2.0+, and later Mono, Xamarin and UWP targets).
+		Console.WriteLine("Testing the polly library with a simple retry pattern");
+		Console.WriteLine("Press a key to end the demo");
 
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Testing the polly library with a simple retry pattern");
-			Console.WriteLine("Press a key to end the demo");
+		var policy = Policy
+			.Handle<MyDatabaseException>()
+			.Retry();
 
-			var policy = Policy
-				.Handle<MyDatabaseException>()
-				.Retry();
-
-			Console.WriteLine("Simulating a action that fails every 5th time.");
-			Console.WriteLine("Polly should retry it if an error occurs.");
-			policy.Execute(
-				delegate()
-				{
-					Console.WriteLine($"Sequence is started from the beginning");
-					do
-					{
-						Do_something_and_simulate_a_fault_every_fifth_time();
-						Thread.Sleep(1000);
-					} while (!Console.KeyAvailable);
-				});
-		}
-
-		private static int _counter = 0;
-
-		private static void Do_something_and_simulate_a_fault_every_fifth_time()
-		{
-			_counter++;
-			Console.WriteLine($"Call no. {_counter}...");
-			if ((_counter % 5) == 0)
+		Console.WriteLine("Simulating a action that fails every 5th time.");
+		Console.WriteLine("Polly should retry it if an error occurs.");
+		policy.Execute(
+			delegate()
 			{
-				Console.WriteLine($"Throwing an exception for demonstration!");
-				throw new MyDatabaseException();
-			}
-		}
-
-		private class MyDatabaseException : Exception {}
+				Console.WriteLine($"Sequence is started from the beginning");
+				do
+				{
+					Do_something_and_simulate_a_fault_every_fifth_time();
+					Thread.Sleep(1000);
+				} while (!Console.KeyAvailable);
+			});
 	}
+
+	private static int _counter = 0;
+
+	private static void Do_something_and_simulate_a_fault_every_fifth_time()
+	{
+		_counter++;
+		Console.WriteLine($"Call no. {_counter}...");
+		if ((_counter % 5) == 0)
+		{
+			Console.WriteLine($"Throwing an exception for demonstration!");
+			throw new MyDatabaseException();
+		}
+	}
+
+	private class MyDatabaseException : Exception {}
 }
