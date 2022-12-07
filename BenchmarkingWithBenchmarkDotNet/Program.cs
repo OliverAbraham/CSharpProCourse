e5c670f40d6726e56b08c3a579d4538d90161297
Oliver Abraham <mail@oliver-abraham.de>
2022-12-07 17:17:28 +0100

    Benchmarking

---
+++ BenchmarkingWithBenchmarkDotNet/Program.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+
+Console.WriteLine("Testing my 'GetYearFromDate'");
+
+BenchmarkRunner.Run<Benchmarks>();
+
+
+[MemoryDiagnoser]
+[OrdererAttribute(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumnAttribute]
+public class Benchmarks
+{
+    private const string _input = "2022-12-24T13:59:59Z";
+    private MyClassIWantToTest parser = new();
+
+    [Benchmark]
+    public void GetYearFromDateTime()
+    {
+        parser.GetYearFromDate(_input);
+    }
+}
+
+public class MyClassIWantToTest
+{
+    // input should be in format "2022-12-24T13:59:59Z"
+    public int GetYearFromDate(string dateTimeAsString)
+    {
+        var datetime = DateTime.Parse(dateTimeAsString);
+        return datetime.Year;
+    }
+}
