1d610fd25cfc21c04f30c93fcf4082cfaf1d93a4
Oliver Abraham <mail@oliver-abraham.de>
2023-01-28 17:24:22 +0100

    added primitive obsession demo

---
+++ PrimitiveObsession1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PrimitiveObsession1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Verkaufspreis berechnen");
+
+            decimal nettopreis = 100.0m;
+            decimal bruttopreis = BerechneBruttopreis(nettopreis);
+            Console.WriteLine($"Der Bruttopreis ist {bruttopreis} Euro");
+
+            decimal steueranteil = BerechneSteueranteil(nettopreis, bruttopreis);
+            Console.WriteLine($"Der Steueranteil ist {steueranteil * 100:N2} %");
+        }
+
+        private static decimal BerechneBruttopreis(decimal nettopreis)
+        {
+            return nettopreis * 1.19m;
+        }
+ 
+        private static decimal BerechneSteueranteil(decimal bruttopreis, decimal nettopreis)
+        {
+            return (bruttopreis-nettopreis) / nettopreis;
+        }
+   }
+}
---
+++ PrimitiveObsession2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace PrimitiveObsession2
+{
+    internal class Program
+    {
+        class Bruttobetrag
+        {
+            private decimal _value;
+            private Bruttobetrag(decimal value) { _value = value; }
+            public static Bruttobetrag FromAmount(decimal value) { return new Bruttobetrag(value); }
+            public decimal ToAmount() { return _value; }
+            public override string ToString() { return _value.ToString(); }
+        }
+        
+        class Nettobetrag
+        {
+            private decimal _value;
+            private Nettobetrag(decimal value) { _value = value; }
+            public static Nettobetrag FromAmount(decimal value) { return new Nettobetrag(value); }
+            public decimal ToAmount() { return _value; }
+            public override string ToString() { return _value.ToString(); }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Verkaufspreis berechnen");
+
+            Nettobetrag nettopreis = Nettobetrag.FromAmount(100.0m);
+            Bruttobetrag bruttopreis = BerechneBruttopreis(nettopreis);
+            Console.WriteLine($"Der Bruttopreis ist {bruttopreis} Euro");
+
+            decimal steueranteil = BerechneSteueranteil(bruttopreis, nettopreis);
+            Console.WriteLine($"Der Steueranteil ist {steueranteil * 100:N2} %");
+        }
+
+        private static Bruttobetrag BerechneBruttopreis(Nettobetrag nettopreis)
+        {
+            var amount = nettopreis.ToAmount() * 1.19m;
+            return Bruttobetrag.FromAmount(amount);
+        }
+ 
+        private static decimal BerechneSteueranteil(Bruttobetrag bruttopreis, Nettobetrag nettopreis)
+        {
+            return (bruttopreis.ToAmount()-nettopreis.ToAmount()) / nettopreis.ToAmount();
+        }
+   }
+}
