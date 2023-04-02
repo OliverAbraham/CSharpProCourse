7a16d7a200c256a5d9286f2c283c9da3b8b5d122
Oliver Abraham <mail@oliver-abraham.de>
2023-04-02 16:14:15 +0200

    Nextcloud client WPF

--- NextcloudClientWpf/MainWindow.xaml.cs
+++ NextcloudClientWpf/MainWindow.xaml.cs
@@ -1,13 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Xml.Linq;
 using WebDav;
 
 namespace NextcloudClientWpf
